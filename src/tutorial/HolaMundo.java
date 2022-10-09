4d049f9a20d8fdb831512d26ea9cb3117cb6270c
ismae <ismae@LAPTOP-223LFIFR>
2022-10-09 17:22:26 +0200

    Creamos la rama 4, que surge del tag 4

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Modificaión la rama 2");
+		System.out.println("Soy la segunda rama que surge del tag 4");
 		//Cambio
 	}
 
