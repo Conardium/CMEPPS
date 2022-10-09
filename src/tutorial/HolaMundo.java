2047e2e129fb4ff50307517b89f6ac232239a538
ismae <ismae@LAPTOP-223LFIFR>
2022-10-09 17:28:14 +0200

    Merge entre rama master y rama4

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -7,6 +7,7 @@ public class HolaMundo {
 		System.out.println("Soy la segunda rama que surge del tag 4");
 		
 		//Modificacion 2 de la rama 4
+		//Se ha hecho un merge entre la rama 4 y la master
 	}
 
 }
