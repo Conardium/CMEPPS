be01aced8980f2f7a04850fe99f73d61f64343b3
ismae <ismae@LAPTOP-223LFIFR>
2022-10-11 16:27:53 +0200

    Primer commit

---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+
+public class Calculadora {
+
+	public int suma(int a, int b) {
+		return a+b;
+	}
+	
+	public int resta(int a, int b) {
+		return a-b;
+	}
+	
+	public int multiplica(int a, int b) {
+		return a*b;
+	}
+	
+	public int divide(int a, int b) {
+		if(b == 0)
+			return -1;
+		else
+			return a/b;
+	}
+}
